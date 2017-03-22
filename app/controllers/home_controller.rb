781eedaf1a4b1c9bc736756203346379955da74a
Denis Gorbunov <denis@nodomain.me>
2017-03-22 14:19:53 +0300

    Перенес проверку авторизации в контроллер

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
+    if logged_in?
+      @user = current_user
+      render 'home/index'
+    else
+      login_page
+    end
+  end
 
+  private
+  def login_page
+    render 'home/login'
   end
 end
