15dca0ca68793d0a3a70c4e27485cd97d3aa5336
Denis Gorbunov <denis@nodomain.me>
2017-03-27 18:53:02 +0300

    TimeZone + some fix

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   match "login" => "user_sessions#new", :as => "login", via: [:get, :post]
   match "logout" => "user_sessions#destroy", :as => "logout", via: [:get, :post]
 
-  get '404', :to => 'application#page_not_found'
+#  get '404', :to => 'application#page_not_found'
 end
